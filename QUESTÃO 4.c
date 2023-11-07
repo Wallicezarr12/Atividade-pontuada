9efae3743430ed97d645ac14082f11d285cefe03
Wallicezarr12 <143550579+Wallicezarr12@users.noreply.github.com>
2023-11-07 14:51:56 -0300

    Add files via upload

---
+++ QUESTÃO 4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int idade;
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+
+    if (idade >= 18) {
+        printf("Acesso permitido \n");
+    } 
+    else {
+        printf("Acesso negado \n");
+    }
+
+
+
+
+    return 0;
+}
